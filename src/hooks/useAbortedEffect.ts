f684aed8800723e1589446d99e30e207f38fe75b
Akashdeep Patra <adeep8961@gmail.com>
2022-08-12 23:59:26 +0530

    refactor examples (#2)

--- src/hooks/useAbortedEffect.ts
+++ src/hooks/useAbortedEffect.ts
@@ -2,17 +2,20 @@ import axios, { CancelToken } from 'axios';
 import { useEffect } from 'react';
 
 const useAbortedEffect = (
-  effect: (
-    signal: AbortSignal,
-    axiosCancelToke: CancelToken
-  ) => Function | void,
+  effect: (params: {
+    signal: AbortSignal;
+    axiosCancelToke: CancelToken;
+  }) => Function | void,
   dependencies: Array<any>
 ) => {
   useEffect(() => {
     const axiosCancelToken = axios.CancelToken.source();
     const abortController = new AbortController();
     const signal = abortController.signal;
-    const cleanupEffect = effect(signal, axiosCancelToken.token);
+    const cleanupEffect = effect({
+      signal,
+      axiosCancelToke: axiosCancelToken.token
+    });
 
     return () => {
       if (cleanupEffect) {
